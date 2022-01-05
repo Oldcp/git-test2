3c0d1eede5c95c68f710e71bf5717b2d2e52937a
oldmoney <1378931851@qq.com>
2022-01-05 16:41:00 +0800

    push commit

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -13,6 +13,7 @@ public class gittest {
         System.out.println("hello git444444!!!");
         System.out.println("master test");
         System.out.println("hot-fix test!!!");
+        System.out.println("push test!!!");
     }
 
 }
