918aa48d4a82d0da0a6a7bb0c770f413d03875b5
oldmoney <1378931851@qq.com>
2022-01-05 15:52:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gittest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author old money
+ * @create 2022-01-05 15:48
+ */
+public class gittest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+
+}
